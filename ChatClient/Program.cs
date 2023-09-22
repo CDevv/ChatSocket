871e0fd4d5d37009f2744b31f511272aabb72840
CDevv <cdevyt@gmail.com>
2023-09-22 11:55:03 +0300

    Add project files.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Chat Client");
+string msg = "";
+
+string localhost = Dns.GetHostName();
+IPHostEntry entry = Dns.GetHostEntry(localhost);
+IPEndPoint endPoint = new(entry.AddressList[0], 11000);
+Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    socket.Connect(endPoint);
+    while (true)
+    {
+        Console.Write(">");
+        msg = Console.ReadLine();
+        byte[] msgBytes = Encoding.ASCII.GetBytes(msg + "<EOF>");
+        socket.Send(msgBytes);
+        if (msg == "exit")
+        {
+            break;
+        }
+    }
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
---
+++ ChatSocket/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Chat Server");
+string msg = "";
+
+string localhost = Dns.GetHostName();
+IPHostEntry entry = Dns.GetHostEntry(localhost);
+IPEndPoint endPoint = new(entry.AddressList[0], 11000);
+Socket socket = new(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    byte[] buffer = new byte[1024];
+    socket.Bind(endPoint);
+    socket.Listen(100);
+    Socket handle = socket.Accept();
+    while (true)
+    {
+        msg = "";
+        while (true)
+        {
+            int msgSize = handle.Receive(buffer);
+            msg = Encoding.ASCII.GetString(buffer, 0, msgSize);
+            msg = msg.Replace("<EOF>", "");
+            Console.WriteLine(">" + msg);
+            if (msg == "exit")
+            {
+                handle.Shutdown(SocketShutdown.Both);
+                handle.Close();
+                break;
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    throw;
+    Console.WriteLine(e.Message);
+}
+
+Console.Clear();
+Console.WriteLine("Goodbye");
